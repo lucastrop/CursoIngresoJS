9fe96124d10d4a3f8d4866ebae406925fbbee49c
Lucía Castro <lulisaric@gmail.com>
2021-08-01 17:58:57 -0300

    ejercicio 4 if

--- 2-InstruccionIf/jsInstruccionIF-04.js
+++ 2-InstruccionIf/jsInstruccionIF-04.js
@@ -1,7 +1,13 @@
 function mostrar()
 {
+	let edad;
+	edad= parseInt(document.getElementById("txtIdEdad").value);
 	//tomo la edad  
-	alert("ok");
+	if  (edad >= 13) {
+		if(edad <=17)
+		alert ("La persona es adolescente");
+	}
+	
 
 
 }//FIN DE LA FUNCIÓN
