a1e4252f7c2170e3da5c0c5524059e0fabb3bcae
Lucía Castro <lulisaric@gmail.com>
2021-08-01 12:59:59 -0300

    ejercicio 10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -5,5 +5,9 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
+	let importe;
+	let resultado;
+importe = parseFloat(document.getElementById("txtIdImporte").value);
+resultado= importe - importe*25/100
+document.getElementById("txtIdResultado").value=resultado;
 }
