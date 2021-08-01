9bd1760bb6f4419c026096346d0f3884a439c4c3
Lucía Castro <lulisaric@gmail.com>
2021-08-01 20:40:48 -0300

    ejercicios if hasta 10

--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,17 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
-
+	let numero;
+	let maximo = 10;
+	let minimo = 1;
+	numero= Math.round(Math.random() * (maximo - minimo) + minimo);
+if(numero>8) {
+	alert("EXCELENTE, nota = " + numero);
+}
+else if (numero>=4) {
+	alert("APROBÓ nota = " + numero);
+}
+else {
+	alert("Vamos, la próxima se puede, nota = " + numero);
+}
 }//FIN DE LA FUNCIÓN
