f6034804c430c7ab40c5171cc6c1f537c9a5c15f
Lucía Castro <lulisaric@gmail.com>
2021-08-01 17:48:00 -0300

    ejercicio 3 if

--- 2-InstruccionIf/jsInstruccionIF-02.js
+++ 2-InstruccionIf/jsInstruccionIF-02.js
@@ -1,7 +1,13 @@
 function mostrar()
 {
+	let edad;
+	edad= parseInt(document.getElementById("txtIdEdad").value);
 	//tomo la edad  
-	alert("ok");
+	if  (edad >= 18) {
+		alert ("La persona es mayor de edad");
+	}
+	
+	
 	
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF-03.js
+++ 2-InstruccionIf/jsInstruccionIF-03.js
@@ -1,6 +1,15 @@
 function mostrar()
 {
 	//tomo la edad  
-	alert("ok");
+	let edad;
+	edad= parseInt(document.getElementById("txtIdEdad").value);
+	//tomo la edad  
+	if  (edad >= 18) {
+		alert ("La persona es mayor de edad");
+	}
+	else{
+		alert ("La persona es menor de edad");
+	}
+	
 
 }//FIN DE LA FUNCIÓN
