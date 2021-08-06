d9948a69cb40b10ab2a0919930a7a64ce77b9fcc
Lucía Castro <lulisaric@gmail.com>
2021-08-05 21:25:10 -0300

    tp 1 if

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -10,5 +10,64 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  */
 function CalcularPrecio () 
 {
- 	
+    const PRECIO=35;
+ 	let cantidad;
+    let marca;
+    let descuento;
+    let importefinal;
+    let preciocondescuento;
+    let iibb;
+    cantidad = parseInt(document.getElementById("txtIdCantidad").value);
+    marca = document.getElementById("Marca").value;
+
+    switch(cantidad){
+    case 1:
+    case 2:
+    descuento=0;
+        break;
+    case 3:
+        if(marca == "FelipeLamparas"){
+            descuento=10;
+        }
+        else if (marca == "ArgentinaLuz"){
+            descuento=15;
+        }
+        else{
+            descuento=5;
+        }
+        break;
+    case 4:
+        if(marca == "ArgentinaLuz" || marca == "FelipeLamparas"){
+            descuento=25;
+        }
+        else{
+            descuento=20;
+        }
+        break;
+    case 5:
+        if(marca =="ArgentinaLuz"){
+           descuento=40;
+        }
+        else{
+            descuento=30;
+        }
+        break;
+  default:
+      descuento= 50;
+break;
+        }
+    alert("descuento es " + descuento);
+preciocondescuento=PRECIO-PRECIO*descuento/100;
+document.getElementById("txtIdprecioDescuento").value= preciocondescuento;
+importefinal=preciocondescuento*cantidad;
+iibb=importefinal*10/100;
+if(importefinal > 120){
+    importefinal += iibb;
+    alert("Importe a pagar $ " + importefinal);
+    alert("IIBB Usted pago " + iibb );    
 }
+else{
+    alert("Importe a pagar $ " + importefinal);
+}
+
+    }
